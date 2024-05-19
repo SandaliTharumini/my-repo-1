333010a2a70d18e115d8e86f65c21042f9e45887
Sandali Tharumini <tharuminiwellehewa@gmail.com>
2024-05-19 18:27:32 +0530

    cat class added

--- file 1.py
+++ file 1.py
@@ -12,3 +12,17 @@ class Dog:
     def set_age(self, age):
         self.age = age
 
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def meow(self):
+        print(f"{self.name} says meow!")
+
+    def get_age(self):
+        return self.age
+
+    def set_age(self, age):
+        self.age = age
+
