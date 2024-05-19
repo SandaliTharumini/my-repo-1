e1937150ef48ed35f41a38f4d4e27e01c86da28d
Sandali Tharumini <tharuminiwellehewa@gmail.com>
2024-05-19 17:49:22 +0530

    Update file 1.py
    class dog updated

--- file 1.py
+++ file 1.py
@@ -5,4 +5,9 @@ class Dog:
 
     def bark(self):
         print(f"{self.name} says woof!")
+        
+    def get_age(self):
+        return self.age
 
+    def set_age(self, age):
+        self.age = age
