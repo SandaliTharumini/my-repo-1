9c4b4622e81126dd649dfb0306d0d972e89cf509
Sandali Tharumini <tharuminiwellehewa@gmail.com>
2024-05-19 17:46:27 +0530

    Update file 1.py
    created dog class

--- file 1.py
+++ file 1.py
@@ -1,26 +1,8 @@
-class TodoList:
-    def __init__(self):
-        self.tasks = []
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-    def add_task(self, task):
-        self.tasks.append(task)
-        print(f"Task '{task}' added.")
+    def bark(self):
+        print(f"{self.name} says woof!")
 
-    def remove_task(self, task):
-        if task in self.tasks:
-            self.tasks.remove(task)
-            print(f"Task '{task}' removed.")
-        else:
-            print(f"Task '{task}' not found.")
-
-    def view_tasks(self):
-        if self.tasks:
-            print("Tasks:")
-            for idx, task in enumerate(self.tasks, start=1):
-                print(f"{idx}. {task}")
-        else:
-            print("No tasks.")
-
-    def clear_tasks(self):
-        self.tasks = []
-        print("All tasks cleared.")
