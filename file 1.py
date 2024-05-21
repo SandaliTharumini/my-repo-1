37cf75c5658bc701a2def772a504df78d307ffdc
Sandali Tharumini <tharuminiwellehewa@gmail.com>
2024-05-21 07:26:36 +0530

    class inherited

--- file 1.py
+++ file 1.py
@@ -1,28 +1,24 @@
-class Dog:
+# pet_animal_definitions.py
+
+class Pet:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-    def bark(self):
-        print(f"{self.name} says woof!")
-        
     def get_age(self):
         return self.age
 
     def set_age(self, age):
         self.age = age
 
-class Cat:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
 
+class Dog(Pet):
+    def bark(self):
+        print(f"{self.name} says woof!")
+
+
+class Cat(Pet):
     def meow(self):
         print(f"{self.name} says meow!")
 
-    def get_age(self):
-        return self.age
-
-    def set_age(self, age):
-        self.age = age
 
